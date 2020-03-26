77e488707ac7d2609c277d3e9aeb362249ad91b2
Christopher jemsek <sjemsek@gmail.com>
2020-03-26 21:29:35 +1100

    connected to maps api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TabPanel from './MaterialTabs.js';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TabPanel from "./MaterialTabs.js";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 class App extends Component {
   render() {
+    const mapStyles = {
+      width: "100%",
+      height: "100%"
+    };
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Material Tab Panel:
-        </p>
-        <TabPanel></TabPanel>
-      </div>
+      <Map
+        google={this.props.google}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{ lat: 47.444, lng: -122.176 }}
+      >
+        <Marker position={{ lat: 48.0, lng: -122.0 }} />
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyB3o5ior_i7n349A_xgUZQbMQ2QAB-XHqg"
+})(App);
