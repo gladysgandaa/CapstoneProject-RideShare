cabc8b93a97e14d077405e316697b19eaeb2ea00
Varun Gupta <s3672583@student.rmit.edu.au>
2020-03-31 18:02:53 +0530

    addded various fields to the booking page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import AddressForm from "./Booking";
+import AddressForm from "./booking";
 import TabPanel from "./MaterialTabs.js";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SignIn from "./SignIn";
--- src/booking.js
+++ src/booking.js
@@ -4,11 +4,15 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/MenuItem";
+import Icon from "@material-ui/core/MenuItem";
+import classes from "@material-ui/core/MenuItem";
 
 export default function AddressForm() {
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
+      <Typography variant="h4" gutterBottom>
         Booking form
       </Typography>
       <Grid container spacing={3}>
@@ -31,63 +35,85 @@ export default function AddressForm() {
             fullWidth
             autoComplete="lname"
           />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="billing address-line1"
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="billing address-level2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-          />
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="phone"
+              name="phone"
+              label="Mobile"
+              fullWidth
+              autoComplete="phone"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email ID"
+              fullWidth
+              autoComplete="email"
+            />
+          </Grid>
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
+            id="date"
+            label="Date of Pick Up"
             fullWidth
-            autoComplete="billing postal-code"
+            type="date"
+            defaultValue="2017-05-24"
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
+            id="time"
+            label="Pick Up Time"
             fullWidth
-            autoComplete="billing country"
+            type="time"
+            defaultValue="07:30"
+            inputProps={{
+              step: 300 // 5 min
+            }}
           />
         </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox color="secondary" name="saveAddress" value="yes" />
-            }
-            label="Use this address for payment details"
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="date"
+            label="Date of Drop Off"
+            fullWidth
+            type="date"
+            defaultValue="2017-05-24"
           />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="time"
+            label="Drop off Time"
+            fullWidth
+            type="time"
+            defaultValue="07:30"
+            inputProps={{
+              step: 300 // 5 min
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField id="select" label="Select Car" fullWidth value="20" select>
+            <MenuItem value="sedan">Sedan</MenuItem>
+            <MenuItem value="suv">SUV</MenuItem>
+          </TextField>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            fullwidth
+            endIcon={<Icon>send</Icon>}
+          >
+            Procced To Payment
+          </Button>
         </Grid>
       </Grid>
     </React.Fragment>
