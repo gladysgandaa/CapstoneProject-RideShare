ecec75ef7db4d004e372318bb78ce2091eb0b391
Christopher jemsek <sjemsek@gmail.com>
2020-04-04 11:44:12 +1100

    arranging merged components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import TabPanel from "./MaterialTabs.js";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import TabPanel from "./MaterialTabs.js";
 import SideList from "./components/SideList";
 import MapContainer from "./components/MapContainer";
 
@@ -13,23 +12,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">Material Tab Panel:</p>
-        <TabPanel></TabPanel>
+        <Router>
+          <TabPanel></TabPanel>
+          <Switch>
+            <Route path="/" component={SideList} exact />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+        </Router>
         <div>
           <MapContainer map={MapContainer} />
         </div>
-        <Router>
-        <TabPanel></TabPanel>
-        <Switch>
-          <Route path="/" component={SideList} exact />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/signup" component={SignUp} />
-        </Switch>
-      </Router>
       </div>
     );
   }
