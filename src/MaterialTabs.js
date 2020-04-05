5ff7e6823261ff158f90d655b3860e7b9be23e85
KyleJ <s3600341@student.rmit.edu.au>
2020-04-05 16:52:03 +1000

    Attempted to fix Routing.

--- src/MaterialTabs.js
+++ src/MaterialTabs.js
@@ -6,8 +6,6 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
