a451150c1d18536f4772541d41ea4882a1c58ebd
kyle_j_1997 <kyle_j_1997@outlook.com>
2020-04-07 15:51:29 +1000

    Restructure Project

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -5,11 +5,11 @@ import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 
-export default function AddressForm() {
+export default function BookingForm() {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Booking form
+        Book a Car.
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
