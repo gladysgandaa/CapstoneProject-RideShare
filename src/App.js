797817561be8b947dbd0ccde68d813157b3c1386
Varun Gupta <s3672583@student.rmit.edu.au>
2020-04-25 14:07:39 +0530

    added post data

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <BookingForm />
         <p className="App-intro">Material Tab Panel:</p>
         <Router>
           <TabPanel></TabPanel>
--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
@@ -7,108 +7,151 @@ import Button from "@material-ui/core/MenuItem";
 import Icon from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    margin: 35
-  }
-}));
+class BookingForm extends Component {
+  constructor(props) {
+    super(props);
 
-export default function BookingForm() {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Typography variant="h4" gutterBottom>
-        Book a Car.
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="fname"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="lname"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="phone"
-            name="phone"
-            label="Mobile"
-            fullWidth
-            autoComplete="phone"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="email"
-            name="email"
-            label="Email ID"
-            fullWidth
-            autoComplete="email"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="select" label="Select Car" fullWidth value="20" select>
-            <MenuItem value="sedan">Sedan</MenuItem>
-            <MenuItem value="suv">SUV</MenuItem>
-          </TextField>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="date"
-            label="Date of Pick Up"
-            fullWidth
-            type="date"
-            defaultValue="2017-05-24"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="time"
-            label="Pick Up Time"
-            fullWidth
-            type="time"
-            defaultValue="07:30"
-            inputProps={{
-              step: 300 // 5 min
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="select"
-            label="Duration (hrs)"
-            fullWidth
-            value="20"
-            select
-          >
-            <MenuItem value="one">1</MenuItem>
-            <MenuItem value="three">3</MenuItem>
-            <MenuItem value="six">6</MenuItem>
-            <MenuItem value="twelve">12</MenuItem>
-            <MenuItem value="twentyFour">24</MenuItem>
-          </TextField>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <Button href="/book" variant="contained" color="primary">
-            Book
-          </Button>
-        </Grid>
-      </Grid>
-    </div>
-  );
+    this.state = {
+      carid: "",
+      date: "",
+      time: "",
+      duration: ""
+    };
+  }
+
+  changeHandler = e => {
+    this.setState({ [e.targwt.name]: e.target.value });
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const useStyles = makeStyles(theme => ({
+      root: {
+        margin: 35
+      }
+    }));
+    const classes = useStyles();
+    const { carid, date, time, duration } = this.state;
+    return (
+      <div className={classes.root}>
+        <form onSubmit={this.submitHandler}>
+          <Typography variant="h4" gutterBottom>
+            Book a Car.
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="firstName"
+                name="firstName"
+                label="First name"
+                fullWidth
+                autoComplete="fname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="lastName"
+                name="lastName"
+                label="Last name"
+                fullWidth
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="phone"
+                name="phone"
+                label="Mobile"
+                fullWidth
+                autoComplete="phone"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="email"
+                name="email"
+                label="Email ID"
+                fullWidth
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="select"
+                name="carid"
+                label="Select Car"
+                fullWidth
+                value={carid}
+                onchange={this.changeHandler}
+                select
+              >
+                <MenuItem value="sedan">Sedan</MenuItem>
+                <MenuItem value="suv">SUV</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="date"
+                name="data"
+                label="Date of Pick Up"
+                fullWidth
+                type="date"
+                value={date}
+                onchange={this.changeHandler}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="time"
+                name="time"
+                label="Pick Up Time"
+                fullWidth
+                type="time"
+                value={time}
+                onchange={this.changeHandler}
+                inputProps={{
+                  step: 300 // 5 min
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="select"
+                name="duration"
+                label="Duration (hrs)"
+                fullWidth
+                value={duration}
+                onchange={this.changeHandler}
+                select
+              >
+                <MenuItem value="one">1</MenuItem>
+                <MenuItem value="three">3</MenuItem>
+                <MenuItem value="six">6</MenuItem>
+                <MenuItem value="twelve">12</MenuItem>
+                <MenuItem value="twentyFour">24</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <Button
+                href="/book"
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                Book
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    );
+  }
 }
