0039638a2af6e1537e3751f0421f22a49dea4e01
gladysganda <gladysganda8@gmail.com>
2020-05-01 17:20:24 +1000

    UI for booking history created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TabPanel from "./components/Navigation/MaterialTabs";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./components/Authentication/SignIn";
 import SignUp from "./components/Authentication/SignUp";
+import History from "./components/History/History";
 import MapContainer from "./components/Map/MapContainer";
 import BookingForm from "./components/Booking/Booking";
 
@@ -41,6 +42,7 @@ class App extends Component {
 
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/history" component={History} />
             <Route path="/book" component={BookingForm} />
           </Switch>
         </Router>
---
+++ src/components/History/History.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  inline: {
+    display: "inline",
+    fontSize: "16px"
+  },
+  root: {
+    margin: "auto"
+  },
+  primary: {
+    fontWeight: "bold"
+  },
+  avatar: {
+    height: "65px",
+    width: "65px",
+    margin: "auto"
+  }
+}));
+
+const History = props => {
+  console.log(props);
+  const classes = useStyles();
+  const { carId, make, model, rentalCostPerHour, distance } = props;
+  return (
+    <div>
+      <h2>Past Booking</h2>
+      <ListItem alignItems="flex-start">
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={1}>
+            <ListItemAvatar>
+              <Avatar alt={model} src="" classes={{ root: classes.avatar }} />
+            </ListItemAvatar>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <ListItemText
+              classes={{ primary: classes.primary }}
+              primary={`${make} ${model}`}
+              secondary={
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={useStyles.inline}
+                  color="textPrimary"
+                >
+                  Booking date: {}
+                  <br></br>
+                  Booking time: {}
+                  <br></br>
+                  Location: {}
+                </Typography>
+              }
+            />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Link
+              to={{
+                pathname: "/book",
+                state: {
+                  make: make,
+                  model: model,
+                  carId: carId
+                }
+              }}
+            >
+              <Button alignItems="flex-end" variant="contained" color="primary">
+                Book Again
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
+      </ListItem>
+      <Divider variant="inset" classes={{ root: classes.root }} />
+    </div>
+  );
+};
+export default History;
