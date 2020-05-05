60bed203dababe673fcadb6e0f77205a37c28109
KyleJ <s3600341@student.rmit.edu.au>
2020-05-05 13:54:48 +1000

    Changed image url to https

--- src/components/Map/MapContainer.jsx
+++ src/components/Map/MapContainer.jsx
@@ -83,7 +83,7 @@ class MapContainer extends Component {
           key={index}
           id={index}
           icon={{
-            url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+            url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
             anchor: new google.maps.Point(0, 53),
             labelOrigin: new google.maps.Point(14, 53)
           }}
