0b3e51dfe42c0e8a07bda1fa5bd87d50fa94cce6
KyleJ <s3600341@student.rmit.edu.au>
2020-05-08 20:39:15 +1000

    AWS Amplify configuration settings

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "rideshare-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: ""
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_o8J5VTRTm",
+    APP_CLIENT_ID: "1iprv2tqs1t9ltt3vlsvmin0vk",
+    IDENTITY_POOL_ID: ""
+  }
+};
