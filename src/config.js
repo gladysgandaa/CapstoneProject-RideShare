518b1fbd401110346641915c4bff4ca62dc4c61c
KyleJ <s3600341@student.rmit.edu.au>
2020-05-08 20:58:25 +1000

    AWS Amplify configuration for authentication

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ export default {
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: ""
+    URL: "https://d8m0e1kit9.execute-api.us-east-1.amazonaws.com/data"
   },
   cognito: {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_o8J5VTRTm",
     APP_CLIENT_ID: "1iprv2tqs1t9ltt3vlsvmin0vk",
-    IDENTITY_POOL_ID: ""
+    IDENTITY_POOL_ID: "us-east-1:4926ba37-c647-4df9-aa7f-80c8b6ecb8a8"
   }
 };
