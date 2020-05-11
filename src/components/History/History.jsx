4dd2411524377c6ecfc7b0d7968bcbaae0334669
gladysganda <gladysganda8@gmail.com>
2020-05-11 19:43:47 +1000

    User booking history implemented

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import HistoryList from "./HistoryList.js";
+import Typography from "@material-ui/core/Typography";
 
 class History extends Component {
   constructor(props) {
@@ -43,6 +44,11 @@ class History extends Component {
     return (
       <div style={useStyles.root}>
         <Grid container spacing={3}>
+          <Grid item xs={12} sm={12}>
+            <Typography component="h1" variant="h4">
+              Booking History
+            </Typography>
+          </Grid>
           <Grid item xs={12} sm={12}>
             <HistoryList bookings={this.state.bookingHistory} />
           </Grid>
--- src/components/History/ShowHistory.js
+++ src/components/History/ShowHistory.js
@@ -8,15 +8,29 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   inline: {
-    display: "inline"
+    display: "inline",
+    fontSize: "16px"
+  },
+  root: {
+    margin: "auto"
+  },
+  primary: {
+    fontWeight: "bold"
+  },
+  avatar: {
+    height: "65px",
+    width: "65px",
+    margin: "auto"
   }
 }));
 
 const ShowHistory = props => {
   console.log(props);
+  const classes = useStyles();
   const {
     startTime,
     bookingId,
@@ -27,51 +41,55 @@ const ShowHistory = props => {
   } = props;
   return (
     <div>
-      <Typography component="h1" variant="h5">
-        Booking History
-      </Typography>
       <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar src="" />
-        </ListItemAvatar>
-        <ListItemText
-          primary={carId}
-          secondary={
-            <Typography
-              component="span"
-              variant="body2"
-              className={useStyles.inline}
-              color="textPrimary"
-            >
-              Booking ID: {carId}
-              <br></br>
-              Booking Time: {startTime}
-              <br></br>
-              Duration : {duration} hours
-              <div>
-                <Link
-                  to={{
-                    pathname: "/book",
-                    state: {
-                      startTime: startTime,
-                      bookingId: bookingId,
-                      pickupLocation: pickupLocation,
-                      carId: carId,
-                      duration: duration,
-                      userId: userId
-                    }
-                  }}
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={1}>
+            <ListItemAvatar>
+              <Avatar src="" classes={{ root: classes.avatar }} />
+            </ListItemAvatar>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <ListItemText
+              classes={{ primary: classes.primary }}
+              primary={carId}
+              secondary={
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={useStyles.inline}
+                  color="textPrimary"
                 >
-                  <Button variant="contained" color="primary">
-                    Book Again
-                  </Button>
-                </Link>
-              </div>
-            </Typography>
-          }
-        />
+                  Booking ID: {carId}
+                  <br></br>
+                  Booking Time: {startTime}
+                  <br></br>
+                  Duration : {duration} hours
+                </Typography>
+              }
+            />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Link
+              to={{
+                pathname: "/book",
+                state: {
+                  startTime: startTime,
+                  bookingId: bookingId,
+                  pickupLocation: pickupLocation,
+                  carId: carId,
+                  duration: duration,
+                  userId: userId
+                }
+              }}
+            >
+              <Button alignItems="flex-end" variant="contained" color="primary">
+                Book Again
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
       </ListItem>
-      <Divider variant="inset" component="li" />
+      <Divider variant="inset" classes={{ root: classes.root }} />
     </div>
   );
 };
