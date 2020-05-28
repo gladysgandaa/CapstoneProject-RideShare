7b3abc41884a0b5502558383f04e9addbf71869a
KyleJ <s3600341@student.rmit.edu.au>
2020-05-28 13:29:55 +1000

    Refactor remove unused imports

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import SideList from "../Map/SideList";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 
