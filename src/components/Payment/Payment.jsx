0c2f6ec985f18e18dd259d572b115fbe0b0ad895
Christopher jemsek <sjemsek@gmail.com>
2020-06-14 23:48:42 +1000

    routing back to home

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import PayPalBtn from "./PayPalBtn";
+import { browserHistory } from "react-router";
+import { withRouter } from "react-router-dom";
 
 export default class PaymentExample extends Component {
   paymentHandler = (details, data) => {
     /** Leave blank since we're not storing payments */
     console.log(details, data);
+    this.goHome();
   };
+
+  goHome() {
+    this.props.history.push("/");
+  }
 
   render() {
     const costPerHour = this.props.location.state.rentalCostPerHour;
